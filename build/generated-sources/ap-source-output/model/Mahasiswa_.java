d6fe171d9e7c6326edbb37080dac5340a68a411c
Adelia08 <khffhadelia@gmail.com>
2022-11-28 22:59:49 +0700

    feat : adding all features data entry

---
+++ build/generated-sources/ap-source-output/model/Mahasiswa_.java
@@ -0,0 +1,16 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2022-11-28T09:01:12")
+@StaticMetamodel(Mahasiswa.class)
+public class Mahasiswa_ { 
+
+    public static volatile SingularAttribute<Mahasiswa, String> nama;
+    public static volatile SingularAttribute<Mahasiswa, String> angkatan;
+    public static volatile SingularAttribute<Mahasiswa, String> sekolahAsal;
+    public static volatile SingularAttribute<Mahasiswa, String> npm;
+
+}
